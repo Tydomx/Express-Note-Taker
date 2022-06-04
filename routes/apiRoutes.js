09f175a698ce05301579a61ba5fe39c6818f8c2f
Tydomx <michaelthai16@gmail.com>
2022-06-04 15:16:28 -0500

    added a small fix

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,23 +4,23 @@ const uuid = require('uuid');
 const router = require('express').Router();
 
 router.get('/notes', (req, res) => {
-    const data = fs.readFile('./db/db.json');
+    const data = fs.readFileSync('./db/db.json');
     res.json(JSON.parse(data));
 });
 
 router.post('/notes', (req, res) =>{
-    const notes = JSON.parse(fs.readFile('./db/db.json'));
+    const notes = JSON.parse(fs.readFileSync('./db/db.json'));
     const addNote = req.body;
     addNote.id = uuid.v4();
     notes.push(addNote);
-    fs.writeFile('./db/db.json', JSON.stringify(notes));
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
     res.json(notes);
 });
 
 router.delete('/notes/:id', (req, res) => {
-    const notes = JSON.parse(fs.readFile('./db/db.json'));
+    const notes = JSON.parse(fs.readFileSync('./db/db.json'));
     const deleteNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id);
-    fs.writeFile('./db/db.json', JSON.stringify(deleteNote));
+    fs.writeFileSync('./db/db.json', JSON.stringify(deleteNote));
 
     res.json(deleteNote);
 });
